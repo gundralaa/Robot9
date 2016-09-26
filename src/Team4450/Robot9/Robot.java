d8caa25f1e38f1a941c5f2b17d2bfad948a8996e
Richard Corn <crossle86@users.noreply.github.com>
2016-09-26 16:42:40 -0700

    Merge summer project branch into master. Summer project did not happen but lots of code tweaks were made while waiting so we wanted to get those changes into master branch.

--- src/Team4450/Robot9/Robot.java
+++ src/Team4450/Robot9/Robot.java
@@ -30,7 +30,7 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class Robot extends SampleRobot 
 {
-  static final String  	PROGRAM_NAME = "RAC9-09.22.16-01";
+  static final String  	PROGRAM_NAME = "RAC9-09.26.16-01";
 
   // Motor CAN ID/PWM port assignments (1=left-front, 2=left-rear, 3=right-front, 4=right-rear)
   CANTalon				LFCanTalon, LRCanTalon, RFCanTalon, RRCanTalon, LSlaveCanTalon, RSlaveCanTalon;
@@ -203,7 +203,7 @@ public class Robot extends SampleRobot
    		monitorBatteryThread = MonitorBattery.getInstance(ds); //new MonitorBattery(ds);
    		monitorBatteryThread.start();
 
-   		monitorCompressorThread =MonitorCompressor.getInstance();	//new MonitorCompressor();
+   		monitorCompressorThread = MonitorCompressor.getInstance();	//new MonitorCompressor();
    		monitorCompressorThread.start();
 
    		// Start camera server using our class for usb cameras.
