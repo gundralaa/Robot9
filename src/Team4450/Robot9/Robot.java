d5d4ce97a35e51dc39da9dcf39716f3e70c4e9a8
gundralaa <mathgame99@gmail.com>
2016-12-02 01:41:28 -0800

    Naming Convention

--- src/Team4450/Robot9/Robot.java
+++ src/Team4450/Robot9/Robot.java
@@ -30,7 +30,7 @@ import edu.wpi.first.wpilibj.Talon;
 
 public class Robot extends SampleRobot 
 {
-  static final String  	PROGRAM_NAME = "RAC9-11.01.16-01";
+  static final String  	PROGRAM_NAME = "AG9-12.02.16-01";
 
   // Motor CAN ID/PWM port assignments (1=left-front, 2=left-rear, 3=right-front, 4=right-rear)
   CANTalon				LFCanTalon, LRCanTalon, RFCanTalon, RRCanTalon, LSlaveCanTalon, RSlaveCanTalon;
